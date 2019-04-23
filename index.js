291e1651fd6fa1d74540e736f0f568f8e7b5bb2e
Carlos Abdalla <abdalla@msdev.com.br>
2019-04-23 16:57:26 -0300

    mend

---
+++ index.js
@@ -0,0 +1,13 @@
+const {
+	initialize,
+	setUserInfo,
+	createSignup
+} = require('./src/react-growsumojs');
+
+const GrowSumo = {
+	initialize,
+	setUserInfo,
+	createSignup
+};
+
+export default GrowSumo;
---
+++ src/react-growsumojs.js
@@ -0,0 +1,33 @@
+export const initialize = key => {
+	const gs = document.createElement('script');
+	gs.src = 'https://snippet.growsumo.com/growsumo.min.js';
+	gs.type = 'text/javascript';
+	gs.async = 'true';
+	gs.onload = gs.onreadystatechange = function() {
+		const rs = this.readyState;
+		if (rs && rs !== 'complete' && rs !== 'loaded') {
+			return;
+		}
+
+		try {
+			growsumo._initialize(key);
+			if (typeof growsumoInit === 'function') {
+				growsumoInit();
+			}
+		} catch (e) {}
+	};
+	const s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gs, s);
+};
+
+export const setUserInfo = ({ name, email, customerKey }) => {
+	growsumo.data.name = name;
+	growsumo.data.email = email;
+	growsumo.data.customer_key = customerKey;
+};
+
+export const createSignup = () => {
+	growsumo.createSignup();
+};
+
+
